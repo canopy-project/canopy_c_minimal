ab15ebe1d113d13b84b55048406a4827f5264c40
Greg Prisament <greg@canopy.link>
2015-03-09 15:59:15 -0700

    Cloud variable and filter interface work

--- include/canopy_min.h
+++ include/canopy_min.h
@@ -121,6 +121,8 @@ typedef enum {
 } canopy_active_status;
 
 
+#define  CANOPY_ACTIVE_STATUS "active_status"
+
 // canopy_ws_connection_status enum identifies whether or not a device is
 // currently connected to the server over websockets.
 typedef enum {
@@ -143,15 +145,27 @@ typedef enum {
     CANOPY_FILTER_WS_CONNECTION_STATUS,
 } filter_term_type;
 
+// Wire format for filters:
+//      
+//    connection_status=connected active_status=active AND temperature>=60 AND
+//
+
+typedef enum {
+    CANOPY_RELATION_OP_INVALID=0,
+    CANOPY_EQ,
+    CANOPY_NEQ,
+    CANOPY_GT,
+    CANOPY_GTE,
+    CANOPY_LT,
+    CANOPY_LTE,
+} canopy_relation_op;
+
 typedef struct filter_term {
-    filter_term_type type;
-
-    union {
-    } constraint;
+    bool is_builtin;
+    char *fieldname;
+    char *value; // TBD should we use canopy_var_value_t here?
+    canopy_relation_op relation;
 }
-
-typedef struct filter_op {
-} or_filter_t;
 
 /*
 {
@@ -458,11 +472,193 @@ canopy_device_query_list(&dq, 10, devices);
 //| username()                                 | -> string
 //| update(params, function(Error)
 
+/*****************************************************************************/
+// CLOUD VARIABLES
+typedef enum {
+    CANOPY_VAR_DIRECTION_INVALID=0,
+    CANOPY_VAR_OUT,
+    CANOPY_VAR_IN,
+    CANOPY_VAR_INOUT,
+} canopy_var_direction;
 
+typedef enum {
+    CANOPY_VAR_DATATYPE_INVALID=0,
+    CANOPY_VAR_DATATYPE_VOID,
+    CANOPY_VAR_DATATYPE_STRING,
+    CANOPY_VAR_DATATYPE_BOOL,
+    CANOPY_VAR_DATATYPE_INT8,
+    CANOPY_VAR_DATATYPE_UINT8,
+    CANOPY_VAR_DATATYPE_INT16,
+    CANOPY_VAR_DATATYPE_UINT16,
+    CANOPY_VAR_DATATYPE_INT32,
+    CANOPY_VAR_DATATYPE_UINT32,
+    CANOPY_VAR_DATATYPE_FLOAT32,
+    CANOPY_VAR_DATATYPE_FLOAT64,
+    CANOPY_VAR_DATATYPE_DATETIME,
+    CANOPY_VAR_DATATYPE_STRUCT,
+    CANOPY_VAR_DATATYPE_ARRAY,
+} canopy_var_datatype;
 
+#define CANOPY_VAR_NAME_MAX_LENGTH 128
+typedef struct canopy_var {
+    canopy_device_t *device;
+    canopy_var_direction direction;
+    canopy_var_datatype type;
+    char name[CANOPY_VAR_NAME_MAX_LENGTH];
+} canopy_var_t;
 
+#define CANOPY_VAR_VALUE_MAX_LENGTH 128
+typedef struct canopy_var_value {
+    canopy_var_datatype type;
+    union {
+        struct {
+            char *buf;
+            size_t len;
+        } val_string;
+        bool val_bool;
+        int8_t val_int8;
+        int16_t val_int16;
+        int32_t val_int32;
+        uint8_t val_uint8;
+        uint16_t val_uint16;
+        uint32_t val_uint32;
+        float val_float;
+        double val_double;
+        struct time_t val_time;
+    } value;
+}
 
+canopy_error canopy_device_var_init(canopy_device_t *device,
+        canopy_var_direction direction,
+        canopy_var_datatype type,
+        const char *name,
+        canopy_var_t *out_var);
 
+canopy_error canopy_var_set_bool(canopy_var_t *var, bool value);
+canopy_error canopy_var_set_int8(canopy_var_t *var, int8_t value);
+canopy_error canopy_var_set_int16(canopy_var_t *var, int16_t value);
+canopy_error canopy_var_set_int32(canopy_var_t *var, int32_t value);
+canopy_error canopy_var_set_uint8(canopy_var_t *var, uint8_t value);
+canopy_error canopy_var_set_uint16(canopy_var_t *var, uint16_t value);
+canopy_error canopy_var_set_uint32(canopy_var_t *var, uint32_t value);
+canopy_error canopy_var_set_datetime(canopy_var_t *var, struct time_t value);
+canopy_error canopy_var_set_float32(canopy_var_t *var, float value);
+canopy_error canopy_var_set_float64(canopy_var_t *var, double value);
+canopy_error canopy_var_set_string(canopy_var_t *var, const char *value, size_t len);
+
+canopy_error canopy_var_get_bool(canopy_var_t *var, 
+        bool *out_value, 
+        unsigned long *out_ms);
+canopy_error canopy_var_get_int8(canopy_var_t *var, 
+        int8_t *out_value, 
+        unsigned long *out_ms);
+canopy_error canopy_var_get_int16(canopy_var_t *var, 
+        int16_t *out_value, 
+        unsigned long *out_ms);
+canopy_error canopy_var_get_int32(canopy_var_t *var, 
+        uint32_t *out_value, 
+        unsigned long *out_ms);
+canopy_error canopy_var_get_uint8(canopy_var_t *var, 
+        uint8_t *out_value, 
+        unsigned long *out_ms);
+canopy_error canopy_var_get_uint16(canopy_var_t *var, 
+        uint16_t *out_value, 
+        unsigned long *out_ms);
+canopy_error canopy_var_get_uint32(canopy_var_t *var, 
+        uint32_t *out_value, 
+        unsigned long *out_ms);
+canopy_error canopy_var_get_datetime(canopy_var_t *var, 
+        struct time_t *out_value, 
+        unsigned long *out_ms);
+canopy_error canopy_var_get_float32(canopy_var_t *var, 
+        float32 out_value, 
+        unsigned long *out_ms);
+canopy_error canopy_var_get_float64(canopy_var_t *var, 
+        float64 out_value, 
+        unsigned long *out_ms);
+canopy_error canopy_var_get_string(canopy_var_t *var, 
+        char *dest, 
+        size_t len,
+        size_t *out_len,
+        unsigned long *out_ms);
+
+/*
+ *  myVar = device.varInit("out", "float32", "temperature");
+ *
+ *  device.updateToServer().onDone(
+ *      function(error, device) {alert("all done");}
+ *  );
+ *
+ *  
+ */
+
+/*
+ *  Variable initialization will be ignored if the variable already exists on
+ *  the server. 
+ *
+ *  You can't change the datatype after initialization.
+ *
+ *  POST api/devices/UUID
+ *  {
+ *      "var_init" : {
+ *          "out int8 coffee_mode" : {},
+ *          "out float32 temperature" : {},
+ *      }
+ *  }
+ *
+ *  POST api/devices/UUID
+ *  {
+ *      "var_init" : {
+ *          "out int8 coffee_mode" : {},
+ *          "out float32 temperature" : {},
+ *      }
+ *      "vars" : {
+ *          "coffee_mode" : 4,
+ *          "temperature" : 100.34,
+ *          "statusmsg" : "hello",
+ *      }
+ *  }
+ *
+ *  GET api/devices/UUID
+ *  {
+ *      "status" : {
+ *          "last_seen" : "2015-03-09 10:44:32.245 UTC",
+ *          "ws_connected" : true,
+ *      }
+ *      "sddl" : {
+ *          "out int8 coffee_mode" : {}
+ *      }
+ *      "vars" : {
+ *          "coffee_mode" : {
+ *              "t" : "2015-03-09 10:44:32.245 UTC",
+ *              "v" : 4
+ *          },
+ *          "statusmsg" : {
+ *              "t" : "2015-03-09 10:44:32.245 UTC",
+ *              "v" : "hello"
+ *          }
+ *      }
+ *  }
+ */
+
+/*
+ * canopy_var_t gpsvar;
+ * canopy_device_var_init(dev, OUT, CANOPY_VAR_DATATYPE_STRUCT, "gps", &gpsvar)
+ *
+ * canopy_var_struct_init_member(
+ *      &gpsvar, 
+ *      CANOPY_VAR_DATATYPE_FLOAT32, 
+ *      "latitude");
+ *
+ * canopy_var_struct_init_member(
+ *      &gpsvar, 
+ *      CANOPY_VAR_DATATYPE_FLOAT32, 
+ *      "longitude");
+ *
+ *
+ * canopy_var_set_int8(var
+ * 
+ */
 
 #if 0
 CanopyResultEnum canopy_init_client(CanopyClient_t *client);
